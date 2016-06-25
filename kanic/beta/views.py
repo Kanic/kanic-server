4bd0d3f5be89943e62dae921c05f18d3185c4f4f
Dong Liang <dongliang3571@gmail.com>
2016-06-25 03:23:23 -0400

    added list tester page

--- kanic/beta/views.py
+++ kanic/beta/views.py
@@ -1,8 +1,12 @@
+import base64
+
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+from django.http import HttpResponse
 from django.shortcuts import render, HttpResponseRedirect
 
 from .forms import SignUpForm
+from .models import Tester
 
 
 def index(request):
@@ -20,3 +24,25 @@ def index(request):
 
 
     return render(request, 'beta/index.html', context)
+
+def listTester(request):
+    uname = "ahmed"
+    pword = "123"
+
+    if 'HTTP_AUTHORIZATION' in request.META:
+        basic, auth = request.META['HTTP_AUTHORIZATION'].split()
+        username, password = base64.b64decode(auth).split(':')
+        if username == uname and password == pword:
+            testers = Tester.objects.all().order_by('-createAt')
+            total = testers.count()
+
+            context = {
+                "testers": testers,
+                "total": total
+            }
+            return render(request, 'beta/listTester.html', context)
+
+    response = HttpResponse()
+    response.status_code = 401
+    response['WWW-Authenticate'] = 'Basic realm="%s"' % "Basci Auth Protected"
+    return response
