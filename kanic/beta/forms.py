719429802b31ad1b609a7ccd715fa4a2483d3e5c
Dong Liang <dongliang3571@gmail.com>
2016-06-25 20:37:40 -0400

    fix form bug

--- kanic/beta/forms.py
+++ kanic/beta/forms.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django import forms
-from django.forms import BooleanField, CharField, EmailField
+from django.forms import BooleanField, CharField, EmailField, ChoiceField
 from django.forms.extras.widgets import SelectDateWidget
 
 from crispy_forms.helper import FormHelper
@@ -23,9 +23,10 @@ class SignUpForm(forms.ModelForm):
     zipCode = CharField(
         widget=forms.TextInput(attrs={'placeholder': '11373'})
     )
-    car = BooleanField(
-        label="Do you have a car?",
-        required=False
+    car = ChoiceField(
+        label="Own a car?",
+        widget=forms.RadioSelect,
+        choices=((True, 'Yes'),(False, 'No'))
     )
 
     class Meta:
@@ -42,12 +43,31 @@ class SignUpForm(forms.ModelForm):
         self.helper.label_class = 'col-bg-2 col-mid-2 col-sm-3 col-xs-3'
         self.helper.field_class = 'col-bg-8 col-mid-8 col-sm-8 col-xs-8'
 
+    def clean_name(self):
+        name = (self.cleaned_data.get('name')).lower()
+        return name
+
     def clean_email(self):
-        email = self.cleaned_data.get('email')
+        email = (self.cleaned_data.get('email')).lower()
         if ('@' not in email) or ('.' not in email):
             raise forms.ValidationError("Must use a valid email")
+        print email
         return email
 
     def clean_phone(self):
         phone = self.cleaned_data.get('phone')
         return phone
+
+    def clean_zipCode(self):
+        zipCode = self.cleaned_data.get('zipCode')
+        if zipCode.isdigit():
+            return zipCode
+        else:
+            raise forms.ValidationError("zip code must be numbers.")
+
+    def clean_car(self):
+        car = self.cleaned_data.get('car')
+        if (car in ['True', True]):
+            return True
+        else:
+            return False
--- kanic/beta/views.py
+++ kanic/beta/views.py
@@ -20,7 +20,6 @@ def index(request):
             messages.success(request, 'Submitted successfully.')
             return HttpResponseRedirect(reverse('index'))
         else:
-            print "not valid"
             return render(request, 'beta/index.html', context)
 
 
