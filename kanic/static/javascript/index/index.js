d50ffc589a4c069e72d25b33da929310cecfdef0
Dong Liang <dongliang3571@gmail.com>
2016-07-31 17:26:49 -0400

    improve middle

--- kanic/static/javascript/index/index.js
+++ kanic/static/javascript/index/index.js
@@ -23,11 +23,19 @@ $(document).ready(function() {
             $('.navbar-default .navbar-nav > li > a').css('color', 'white');
             $('.navbar-default .navbar-brand').css('color', 'white');
         }
+
+        if(top > 395) {
+            $('div.left').css('-webkit-transform', 'translateX(0px)');
+            $('div.middle').css('-webkit-transform', 'translateY(0px)');
+            $('div.right').css('-webkit-transform', 'translateX(0px)');
+        }
     });
 
     // trigger css in main section to animate(eg. transition)
     $('.landing-section').css('top', '40%');
-    $('.main-line, .main-title').css('color', 'rgba(255, 255, 255, 1)')
-    $('button.signup').css('color', 'rgba(0, 0, 0, 0.4)')
-    $('button.signup').css('background-color', 'rgba(255, 255, 255, 1)')
+    $('.main-line, .main-title').css('color', 'rgba(255, 255, 255, 1)');
+    $('button.signup').css('color', 'rgba(0, 0, 0, 0.4)');
+    $('button.signup').css('background-color', 'rgba(255, 255, 255, 1)');
+
+
 });
